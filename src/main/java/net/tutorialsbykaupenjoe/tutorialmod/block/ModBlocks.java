23575f6edb29fb9025987f4fc771ab83b8404e98
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-10-20 18:49:34 +0200

    added tooltips

--- src/main/java/net/tutorialsbykaupenjoe/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/tutorialsbykaupenjoe/tutorialmod/block/ModBlocks.java
@@ -1,8 +1,9 @@
 package net.tutorialsbykaupenjoe.tutorialmod.block;
 
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -15,6 +16,8 @@ import net.tutorialsbykaupenjoe.tutorialmod.block.custom.SpeedyBlock;
 import net.tutorialsbykaupenjoe.tutorialmod.item.ModCreativeModeTab;
 import net.tutorialsbykaupenjoe.tutorialmod.item.ModItems;
 
+import javax.annotation.Nullable;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class ModBlocks {
@@ -22,7 +25,8 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
     public static final RegistryObject<Block> TITANIUM_BLOCK = registerBlock("titanium_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(12f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MATERIALS);
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(12f).requiresCorrectToolForDrops()),
+            "tooltip.block.tutorialmod.titanium_block");
 
     public static final RegistryObject<Block> TITANIUM_ORE = registerBlock("titanium_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(10f).requiresCorrectToolForDrops()));
@@ -30,6 +34,23 @@ public class ModBlocks {
     public static final RegistryObject<Block> SPEEDY_BLOCK = registerBlock("speedy_block",
             () -> new SpeedyBlock(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()));
 
+
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, String tooltipKey) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tooltipKey);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block, String tooltipKey) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)) {
+            @Override
+            public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flagIn) {
+                tooltip.add(new TranslatableComponent(tooltipKey));
+                super.appendHoverText(stack, level, tooltip, flagIn);
+            }
+        });
+    }
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
--- src/main/java/net/tutorialsbykaupenjoe/tutorialmod/item/custom/SmartBlowtorchItem.java
+++ src/main/java/net/tutorialsbykaupenjoe/tutorialmod/item/custom/SmartBlowtorchItem.java
@@ -2,12 +2,16 @@ package net.tutorialsbykaupenjoe.tutorialmod.item.custom;
 
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.Util;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -15,6 +19,8 @@ import net.minecraft.world.level.block.Blocks;
 import net.tutorialsbykaupenjoe.tutorialmod.block.ModBlocks;
 import net.tutorialsbykaupenjoe.tutorialmod.item.ModItems;
 
+import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Map;
 
 public class SmartBlowtorchItem extends Item {
@@ -52,6 +58,18 @@ public class SmartBlowtorchItem extends Item {
         }
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel,
+                                List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        if(Screen.hasShiftDown()) {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.tutorialmod.blowtorch"));
+        } else {
+            pTooltipComponents.add(new TranslatableComponent("tooltip.tutorialmod.blowtorch_shift"));
+        }
+
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
     private boolean canBlowTorch(Block block) {
