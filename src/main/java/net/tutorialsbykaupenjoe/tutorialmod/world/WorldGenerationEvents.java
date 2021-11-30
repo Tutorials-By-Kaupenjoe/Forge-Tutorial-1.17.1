21e13d4fe16b889bd95ece6d31d88e841a40616f
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-30 21:44:21 +0100

    flower gen

--- src/main/java/net/tutorialsbykaupenjoe/tutorialmod/world/WorldGenerationEvents.java
+++ src/main/java/net/tutorialsbykaupenjoe/tutorialmod/world/WorldGenerationEvents.java
@@ -4,6 +4,7 @@ import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.tutorialsbykaupenjoe.tutorialmod.TutorialMod;
+import net.tutorialsbykaupenjoe.tutorialmod.world.gen.ModFlowerGeneration;
 import net.tutorialsbykaupenjoe.tutorialmod.world.gen.ModTreeGeneration;
 
 @Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID)
@@ -11,5 +12,6 @@ public class WorldGenerationEvents {
     @SubscribeEvent
     public static void ModWorldGeneration(final BiomeLoadingEvent event) {
         ModTreeGeneration.generateTrees(event);
+        ModFlowerGeneration.generateFlowers(event);
     }
 }
--- src/main/java/net/tutorialsbykaupenjoe/tutorialmod/world/features/ModConfiguredFeatures.java
+++ src/main/java/net/tutorialsbykaupenjoe/tutorialmod/world/features/ModConfiguredFeatures.java
@@ -2,10 +2,13 @@ package net.tutorialsbykaupenjoe.tutorialmod.world.features;
 
 import net.minecraft.core.Registry;
 import net.minecraft.data.BuiltinRegistries;
+import net.minecraft.data.worldgen.Features;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.blockplacers.SimpleBlockPlacer;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
@@ -23,6 +26,11 @@ public class ModConfiguredFeatures {
                     new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 3),
                     new TwoLayersFeatureSize(1, 0, 1)).build()));
 
+    public static final ConfiguredFeature<?, ?> ORCHID_CONFIG = Feature.FLOWER.configured((
+                    new RandomPatchConfiguration.GrassConfigurationBuilder(
+                            new SimpleStateProvider(ModBlocks.ORCHID.get().defaultBlockState()),
+                            SimpleBlockPlacer.INSTANCE)).tries(12).build())
+            .decorated(Features.Decorators.HEIGHTMAP_WITH_TREE_THRESHOLD_SQUARED).count(3);
 
     private static <FC extends FeatureConfiguration>ConfiguredFeature<FC, ?> register(String name,
                                                                                       ConfiguredFeature<FC, ?> configuredFeature) {
